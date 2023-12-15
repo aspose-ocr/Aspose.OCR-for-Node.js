4f27571d8759d46aa9ec9dfc6cdcb4dfd8ad27b7
Игорь Фурин <furinforjob@gmail.com>
2023-12-15 09:27:47 +0300

    prepared sample

--- sample.js
+++ sample.js
@@ -1,36 +1,22 @@
-const Module = require("./path/to/module/asposeocr");
-var fs = require('fs');
+const Module = require("./asposeocr");
+var fs = require("fs");
 
-Module.onRuntimeInitialized = async _ =>  
-{
-	fs.readFile('path/to/lic/file.lic', function (err, lic_data) {
-		if (err) {
-		throw err; 
-		}
-		const fileData = new Uint8Array(lic_data);
-		Module.AsposeOCRSetLicenseFromData(fileData);
-		console.log("license is setted " + Module.AsposeOCRGetState());
-
-		fs.readFile('path/to/img/file.png', function (err, img_data) {
-			if (err) {
-				throw err; 
-			}
-			const fileData = new Uint8Array(img_data);
-			let filename = "my_file_name";
-			let stream = Module.FS.open(filename, 'w+');
-			Module.FS.write(stream, fileData, 0, fileData.length, 0);
-			Module.FS.close(stream)
+Module.onRuntimeInitialized = async _ => {
+	fs.readFile("source.png", (err, imageData) => {
+		const imageBytes = new Uint8Array(imageData);
+		let internalFileName = "temp";
+		let stream = Module.FS.open(internalFileName, "w+");
+		Module.FS.write(stream, imageBytes, 0, imageBytes.length, 0);
+		Module.FS.close(stream)
 ;
-
-			var input = Module.WasmAsposeOCRInput();
-			var inputs = new Module.WasmAsposeOCRInputs();
-			var settings = Module.WasmAsposeOCRRecognitionSettings();
-			settings.detect_areas_mode = Module.DetectAreasMode.DOCUMENT;
-			input.url = filename;
-			inputs.push_back(input);
-			var result = Module.AsposeOCRRecognize(inputs, settings);
-			var result_str = Module.AsposeOCRSerializeResult(result, Module.ExportFormat.text);
-			console.log(result_str);
-		});
+		let source = Module.WasmAsposeOCRInput();
+		source.url = internalFileName;
+		let batch = new Module.WasmAsposeOCRInputs();
+		batch.push_back(source);
+		let recognitionSettings = Module.WasmAsposeOCRRecognitionSettings();
+		recognitionSettings.language_alphabet = Module.Language.ENG;
+		var result = Module.AsposeOCRRecognize(batch, recognitionSettings);
+		var text = Module.AsposeOCRSerializeResult(result, Module.ExportFormat.text);
+		console.log(text);
 	});
 }
